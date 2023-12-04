b2f9570628ebed43a08c4baa1c6bcd0f94ff50cf
jovan <jovancraft@gmail.com>
2023-12-04 14:01:48 -0600

    trying to get the initial structure right

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { Route, Routes, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <h2>Client Auth Project</h2>
+      <header>
+      <h2>Friends Database</h2>
+        <Link className='link' to='/login'>Login</Link>
+        <Link className='link' to='/friends'>Friends List</Link>
+        <Link className='link' to='/friends/add'>Add Friends</Link>
+        <Link className='link' to='/logout'>Logout</Link>
+      </header>
+      <Routes>
+        <>
+          <Route path='/' element={<Login />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/friends' element={<FriendsList />} />
+          <Route path='/friends/add' element={<AddFriends />} />
+          <Route path='/logout' element={<Logout />} />
+        </>
+      </Routes>
     </div>
   );
 }
