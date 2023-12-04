a6d2668df26a6736cc9deeef8cfad4b37bdc1a79
jovan <jovancraft@gmail.com>
2023-12-04 14:19:40 -0600

    got my routes to work correctly

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './App.css';
 import { Route, Routes, Link } from 'react-router-dom';
+import Login from './components/Login'
+import FriendsList from './components/FriendsList';
+import AddFriends from './components/AddFriends';
 
 function App() {
   return (
@@ -18,7 +21,7 @@ function App() {
           <Route path='/login' element={<Login />} />
           <Route path='/friends' element={<FriendsList />} />
           <Route path='/friends/add' element={<AddFriends />} />
-          <Route path='/logout' element={<Logout />} />
+          {/* <Route path='/logout' element={<Logout />} /> */}
         </>
       </Routes>
     </div>
---
+++ src/components/AddFriends.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const AddFriends = () => {
+    return (<h2>AddFriends</h2>)
+}
+
+
+
+export default AddFriends
