a8a10923fa02d93309c905721d833df0ad4683ee
jovan <jovancraft@gmail.com>
2023-12-04 18:21:31 -0600

    got addFriends to work correctly

--- src/components/AddFriends.js
+++ src/components/AddFriends.js
@@ -1,22 +1,55 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 const AddFriends = () => {
+
+    const navigate = useNavigate();
+    const [ formData, setFormData ] = useState({
+        name: '',
+        age: '',
+        email: ''
+    })
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const token = localStorage.getItem('token');
+        axios.post(`http://localhost:9000/api/friends`, formData, {
+            headers: {
+                authorization : token
+            }
+        })
+        .then(res => {
+            navigate('/friends')
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
+
+    console.log(formData)
     return (
         <div>
             <h2>AddFriends</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div>
-                    <label htmlFor='username'>Username:</label>
-                    <input id='username' />
+                    <label htmlFor='name'>Name:</label>
+                    <input onChange={handleChange} id='name' name='name'/>
                 </div>
                 <div>
                     <label htmlFor='age'>Age:</label>
-                    <input id='age' />
+                    <input onChange={handleChange} id='age' name='age'/>
                 </div>
                 <div>
                     <label htmlFor='email'>Email:</label>
-                    <input id='email' />
+                    <input onChange={handleChange} id='email' name='email'/>
                 </div>
                 <button>Submit</button>
             </form>
